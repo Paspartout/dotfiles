8ffa42a86772554cd8b6ec18b668ddcc2a33cc4c
Paspartout <paspartout@fogglabs.de>
2015-05-09 10:38:52 +0200

    Add PAGER=less export

--- .mksh/exports.sh
+++ .mksh/exports.sh
@@ -1,5 +1,6 @@
 export VISUAL=vim
 export EDITOR="$VISUAL"
+export PAGER=less
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export PKG_CONFIG_PATH=$LD_LIBRARY_PATH:/usr/local/lib/pkgconfig
